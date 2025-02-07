4659d51c450d7ea150b092d7467cf23cd3b599e8
Meet Amipara <137859840+Mitupatel18@users.noreply.github.com>
2025-02-07 19:26:49 +0530

    Add files via upload

---
+++ src/Components/RedirectPage/RedirectPage.jsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./RedirectPage.css";
+import Redirect from "../../Assets/logo/logo-animation.jpg";
+
+const RedirectPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      const timer = setTimeout(() => {
+        navigate("/home", { replace: true });
+      }, 3100); // 3 seconds delay
+
+      return () => clearTimeout(timer);   
+  }, [navigate]
+);
+
+  return (
+    <div className="redirecte">
+        <div className="img-containor">
+            <img src={Redirect} alt="logo-animation" />
+        </div>
+    </div>
+  )
+};
+
+export default RedirectPage;

256a9d606d00f4175441613716d2c7098adffca2
Meet Amipara <137859840+Mitupatel18@users.noreply.github.com>
2025-02-07 19:30:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
 import HomePage from "./Components/HomePage/HomePage";
@@ -9,24 +9,39 @@ import FertilizerPage from "./Components/ProductPage/FertilizerPage";
 import PGRPage from "./Components/ProductPage/PGRPage";
 import HerbicidePage from "./Components/ProductPage/HerbicidePage";
 import FungicidePage from "./Components/ProductPage/FungicidePage";
+import RedirectPage from "./Components/RedirectPage/RedirectPage";
+
+
+function Layout() {
+  const location = useLocation();
+  
+  // Hide Navbar & Footer on HomePage ("/")
+  const hideNavbarFooter = location.pathname === "/";
+
+  return (
+    <>
+      {!hideNavbarFooter && <Navbar />}
+      <Routes>
+        <Route path="/" element={<RedirectPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/insecticide" element={<InsecticidePage />} />
+        <Route path="/products/fungicide" element={<FungicidePage />} />
+        <Route path="/products/herbicide" element={<HerbicidePage />} />
+        <Route path="/products/pgr" element={<PGRPage />} />
+        <Route path="/products/fertilizer" element={<FertilizerPage />} />
+      </Routes>
+      {!hideNavbarFooter && <Footer />}
+    </>
+  );
+}
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/products/insecticide" element={<InsecticidePage />} />
-          <Route path="/products/fungicide" element={<FungicidePage />} />
-          <Route path="/products/herbicide" element={<HerbicidePage />} />
-          <Route path="/products/pgr" element={<PGRPage />} />
-          <Route path="/products/fertilizer" element={<FertilizerPage />} />
-        </Routes>
-      <Footer />
+      <Layout />
     </BrowserRouter>
-      
   );
 }
 
